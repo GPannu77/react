fa96295e3e5c07a9c61edc266e7c1ec9d9b958b4
Thebossnoor <gurnoor4771@gmail.com>
2026-09-10 11:24:18 -0400

    completed digital clock

--- digital-clock-app/src/DigitalClock.jsx
+++ digital-clock-app/src/DigitalClock.jsx
@@ -19,16 +19,20 @@ function DigitalClock(){
         let hours = time.getHours();
         const minutes = time.getMinutes();
         const seconds = time.getSeconds();
-        const meridiem = hours >= 12 ? "PM":"AM";
+        const meridiem = hours >= 12 ? "PM" : "AM";
 
         hours = hours % 12 || 12;
 
-        return `${hours}:${minutes}:${seconds} ${meridiem}`;
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
+    }
+
+    function padZero(number){
+        return number < 10 ? "0" : "" + number;
     }
 
     return(<div className='clock-container'>
                 <div className='clock'>
-                    <span>00:00:00</span>
+                    <span>{formatTime()}</span>
                 </div>
            </div>)
 }

3c9f793438c83fe22b082be1fc4485a269f24ac6
Thebossnoor <gurnoor4771@gmail.com>
2026-09-11 17:41:51 -0400

    updated functions

--- stop-watch/src/Stopwatch.jsx
+++ stop-watch/src/Stopwatch.jsx
@@ -12,15 +12,17 @@ function Stopwatch() {
     }, [isRunning]);
 
     function start(){
-
+        setIsRunning(true);
+        startTimeRef.current = Date.now(); - elapsedTime;
     }
 
     function stop(){
-
+        setIsRunning(false);
     }
 
     function reset() {
-        
+        setElapsedTime(0);
+        setIsRunning(false);
     }
 
     function formatTime(){

e132c60d9dca3b5cc53a2a3bdb3eba204f1d866c
Thebossnoor <gurnoor4771@gmail.com>
2026-09-15 11:22:16 -0400

    added active conversation selection with useState

--- react-chat-practice/src/App.jsx
+++ react-chat-practice/src/App.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import { currentUser, users, conversations } from "./data/mockData";
 
 function App() {
-  console.log(conversations);
 
-  return(<div>App is working</div>);
+  const[activeConversationId, setActiveConversationId] = useState(null);
+
+  return(
+  <div>
+    <h1>Chats</h1>
+    <ul>
+      {conversations.map((c) => {
+        const participant = users.find((u) => u.id === c.participantId);
+        return (
+        <li 
+          key={c.participantId}
+          onClick={() => setActiveConversationId(c.id)}
+            >
+              {participant.name}
+          </li>);
+      })}
+    </ul>
+
+    <p>Active conversation: {activeConversationId ?? "none selected"}</p>
+  </div>
+  );
 }
 
 

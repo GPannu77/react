07a98b112b209e8a0846eec663b4058912087d47
Thebossnoor <gurnoor4771@gmail.com>
2026-09-16 11:56:27 -0400

    updated imports

--- react-chat-practice/src/App.jsx
+++ react-chat-practice/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { currentUser, users, conversations as initialConversations } from "./data/mockData";
 
 
